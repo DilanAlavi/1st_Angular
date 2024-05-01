df7d1f4f4ea12cde3a26741d928d5b0fd2d8c3fe
DILAN ALVARO ALAVI GUTIERREZ <dilanalavi11@gmail.com>
2024-05-01 09:35:47 -0400

    Se configura app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,15 @@
 import { Routes } from '@angular/router';
 import { InicioComponent } from './paginas/inicio/inicio.component';
+import { TiendaComponent } from './paginas/tienda/tienda.component';
+import { NosotrosComponent } from './paginas/nosotros/nosotros.component';
+import { ContactanosComponent } from './paginas/contactanos/contactanos.component';
+import { PaginaNoEncontradaComponent } from './paginas/pagina-no-encontrada/pagina-no-encontrada.component';
 
 export const routes: Routes = [
-    {path:'inicio',component:InicioComponent}
+    {path:'inicio',component:InicioComponent},
+    {path:'tienda',component:TiendaComponent},
+    {path:'nosotros',component:NosotrosComponent},
+    {path:'contactanos',component:ContactanosComponent},
+    {path:'**',component:PaginaNoEncontradaComponent},
+    {path: '',redirectTo:'inicio',pathMatch:'full'}
 ];
